4a51b072c064ab90c3b6cb00a4fe663137a7b157
Jubair Ahammed Tolon <zs.tolon0073@gamil.com>
2023-03-06 02:40:50 +0600

    firebase init

--- firebase.init.js
+++ firebase.init.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: "AIzaSyBpCS3D-kRfxMIOwDnzarToS-99itNcwbs",
+    authDomain: "tesecase-healthos.firebaseapp.com",
+    projectId: "tesecase-healthos",
+    storageBucket: "tesecase-healthos.appspot.com",
+    messagingSenderId: "184613159083",
+    appId: "1:184613159083:web:fc5fb04be92dfabf6cb7ee",
+    measurementId: "G-94K017LPPC"
 };
 
 // Initialize Firebase
